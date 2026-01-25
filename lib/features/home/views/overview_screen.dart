ed6b80e39e580e08c550f276bc395bed5612c3b1
khalef-khalil <khalef.b.khalil@gmail.com>
2026-01-25 16:25:05 +0100

    Fix: Add go_router import to overview_screen

--- lib/features/home/views/overview_screen.dart
+++ lib/features/home/views/overview_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import '../viewmodels/home_viewmodel.dart';
 import '../widgets/weekly_chart_card.dart';
 import '../widgets/insights_card.dart';
